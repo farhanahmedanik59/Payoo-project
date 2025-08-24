9ccd4229edec12f9e0f63d53e7a7229c1d62bb28
farhanahmedanik59 <anikianiki21@gmail.com>
2025-08-25 01:00:49 +0600

    add functionality in website

--- script/home.js
+++ script/home.js
@@ -1,6 +1,12 @@
+// reusable function
+function getInputValueNumber(id) {
+  value = parseInt(document.getElementById(id).value);
+  return value;
+}
 // Add money feature---------
+const validPin = 1234;
+
 document.getElementById("addMoneyBtn").addEventListener("click", function (event) {
-  const validPin = 1234;
   event.preventDefault();
   const bank = document.getElementById("bank").value;
   const accountNumber = document.getElementById("accountNumber").value;
@@ -9,7 +15,7 @@ document.getElementById("addMoneyBtn").addEventListener("click", function (event
   const cartValue = document.getElementById("cart-value");
   const converted = parseInt(cartValue.innerText);
   if (accountNumber.length < 11) {
-    alert("pin must be 11 digit");
+    alert(" account number must be 11 digit");
     return;
   }
   if (pinNumber !== validPin) {
@@ -19,7 +25,21 @@ document.getElementById("addMoneyBtn").addEventListener("click", function (event
   const totalNewBalance = converted + addMoney;
   cartValue.innerText = totalNewBalance;
 });
-
+// Cash out Feature
+document.getElementById("cashout-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+  const pin = parseInt(document.getElementById("pin-number").value);
+  if (pin !== validPin) {
+    alert("Invalid Pin");
+    return;
+  }
+  const ammount = getInputValueNumber("ammount");
+  const cart = document.getElementById("cart-value");
+  const cartAmmount = parseInt(document.getElementById("cart-value").innerText);
+  console.log(cartAmmount);
+  const newAmmount = cartAmmount - ammount;
+  cart.innerText = newAmmount;
+});
 // toggling feature
 document.getElementById("add-money-box").addEventListener("click", function () {
   const sections = document.getElementsByClassName("feature-section");
